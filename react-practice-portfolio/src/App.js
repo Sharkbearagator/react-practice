e3fcd64bc3a6d099a4021d008687f1a8456a947a
Tyler Nichols <tyler_xboxking_98@msn.com>
2022-11-16 13:00:35 -0700

    Create React App

---
+++ react-practice-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
