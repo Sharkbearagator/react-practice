d22e169af19a162b0ff488777e4b9f3b75b9ed5e
Tyler Nichols <tyler_xboxking_98@msn.com>
2022-11-16 13:18:35 -0700

    Starting again

--- react-practice-portfolio/src/components/header.js
+++ react-practice-portfolio/src/components/header.js
@@ -1,18 +1,13 @@
-import Nav from "react-bootstrap/Nav";
+import React from "react";
 
 function ListExample() {
   return (
-    <Nav defaultActiveKey="/home" as="ul">
-      <Nav.Item as="li">
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <div className="container">
+      <div className="row d-flex flex-direction-row">
+        <div className="name"> Tyler Nichols</div>
+        <div className="About Me"> About Me</div>
+      </div>
+    </div>
   );
 }
 
